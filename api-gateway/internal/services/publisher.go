7c3c482d322b50b19d830e4a94caa3bf4400c510
mohammad safarali <mohammad.safarali@snapp.cab>
2025-08-22 18:55:20 +0330

    fix: refactor api gateway

---
+++ api-gateway/internal/services/publisher.go
@@ -0,0 +1,18 @@
+package services
+
+import (
+	"context"
+	"time"
+)
+
+type Msg struct {
+	CompanyID int64
+	Content   string
+	Receiver  string
+	timestamp time.Time
+	Price     int64
+}
+
+type Publisher interface {
+	Publish(ctx context.Context, msg Msg) error
+}
